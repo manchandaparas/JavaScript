2e35b15c07b9af08985c8ef31c97c588fe1e092b
Paras Manchanda <117076556+manchandaparas@users.noreply.github.com>
2023-01-14 00:39:29 +0530

    Create second code.js

---
+++ second code.js
@@ -0,0 +1,10 @@
+// nn bb ss u
+let a = null; // null
+let b = 345; // number
+let c = true ; // byte
+let d = BigInt("500") + BigInt("900") // boolean
+let e = ("Paras") // string
+let f = Symbol("this is symbol") // symbol
+let g  // undefine
+console.log(a , b, c, d, e, f, g)
+console.log(typeof c)
